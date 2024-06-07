eb37de4983504b1c99429d4185c6a577977c3449
AndrewPamer <AndrewPamer@my.unt.edu>
2024-06-06 20:07:08 -0500

    middleware test

--- src/middleware.js
+++ src/middleware.js
@@ -1,44 +1,41 @@
-import { updateSession } from "@/utils/supabase/middleware";
-import { NextResponse } from "next/server";
+// import { updateSession } from "@/utils/supabase/middleware";
+// import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  try {
-    const { supabase, result } = await updateSession(request);
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-
-    const notProtected =
-      request.nextUrl.pathname === "/" ||
-      request.nextUrl.pathname === "/login" ||
-      request.nextUrl.pathname === "/signup";
-
-    if (user) {
-      if (notProtected) {
-        return NextResponse.rewrite(new URL("/home", request.url));
-      }
-    } else {
-      if (!notProtected) {
-        return NextResponse.rewrite(new URL("/", request.url));
-      }
-    }
-
-    return result;
-  } catch (error) {
-    console.error("middleware error: ", error);
-    return NextResponse.error();
-  }
+  //   try {
+  //     const { supabase, result } = await updateSession(request);
+  //     const {
+  //       data: { user },
+  //     } = await supabase.auth.getUser();
+  //     const notProtected =
+  //       request.nextUrl.pathname === "/" ||
+  //       request.nextUrl.pathname === "/login" ||
+  //       request.nextUrl.pathname === "/signup";
+  //     if (user) {
+  //       if (notProtected) {
+  //         return NextResponse.rewrite(new URL("/home", request.url));
+  //       }
+  //     } else {
+  //       if (!notProtected) {
+  //         return NextResponse.rewrite(new URL("/", request.url));
+  //       }
+  //     }
+  //     return result;
+  //   } catch (error) {
+  //     console.error("middleware error: ", error);
+  //     return NextResponse.error();
+  //   }
 }
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico (favicon file)
+//      * Feel free to modify this pattern to include more paths.
+//      */
+//     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+//   ],
+// };
