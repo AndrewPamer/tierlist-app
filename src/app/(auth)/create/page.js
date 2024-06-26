7cd82dacd5afd3a1e552b3cdbfb647302037312e
AndrewPamer <AndrewPamer@my.unt.edu>
2024-06-25 19:58:10 -0500

    Added adding and removing from list

--- src/app/(auth)/create/page.js
+++ src/app/(auth)/create/page.js
@@ -3,7 +3,11 @@ import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import Image from "next/image";
 
+import { ToastContainer, toast, Flip } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import getSpotifyToken from "@/tools/getSpotifyToken";
+
 export default function Create() {
   const [loading, setLoading] = useState(true);
   const [accessToken, setAccessToken] = useState(null);
@@ -59,13 +63,17 @@ export default function Create() {
 
   function addToList(item) {
     if (list.some((listEl) => listEl.id === item.id)) {
-      console.error("Item already in list");
+      toast.warn(`${item.name} is already in the list`);
     } else {
       setList((prevList) => [...prevList, item]);
+      //toast(`Added ${item.name} to the list`);
     }
   }
 
-  console.log(list);
+  function removeFromList(item) {
+    setList((prevList) => prevList.filter((listEl) => listEl.id !== item.id));
+    //toast(`Removed ${item.name} from the list`);
+  }
 
   if (loading) {
     return "Loading...";
@@ -73,7 +81,66 @@ export default function Create() {
 
   return (
     <main>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2500}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Flip}
+      />
+
       <h1 className="text-3xl font-bold text-center">Create a new List</h1>
+      <form className="">
+        <label className="flex flex-col gap-1">
+          <span className="">List Name</span>
+          <input className="bg-input-bg rounded-xl border-2 border-text p-1.5 outline-none"></input>
+        </label>
+        <button
+          className="font-bold bg-button-bg text-button-text text-1xl p-2.5 rounded-xl hover:bg-button-hover "
+          // disabled={isSubmitting || isSubmitted}
+        >
+          Create List
+        </button>
+      </form>
+      {list.length > 0 ? (
+        <section>
+          <h2>Items</h2>
+          <div className="grid grid-cols-2 gap-2 max-h-72	overflow-scroll">
+            {list.map((item) => {
+              return (
+                <button
+                  className="bg-alt-bg p-2 rounded-md text-left flex flex-col"
+                  onClick={() => removeFromList(item)}
+                  key={item.id}
+                >
+                  <img
+                    className="mb-1"
+                    src={item.images[0].url}
+                    width={640}
+                    height={640}
+                  />
+                  <h3 className="text-lg font-bold">{item.name}</h3>
+                  <div>
+                    {item.artists.map((artist) => {
+                      return (
+                        <p className="text-sm" key={artist.id}>
+                          {artist.name}
+                        </p>
+                      );
+                    })}
+                  </div>
+                </button>
+              );
+            })}
+          </div>
+        </section>
+      ) : null}
       <form
         className="flex flex-col justify-center items-center mb-8"
         onSubmit={handleSubmit((data) => seachForAlbum(data))}
